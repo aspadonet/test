d0e984d2cc4bd1abdae9d2f3e67ee47923bfb59b
aspadonet <aspadonet@gmail.com>
2018-10-25 12:04:11 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include "widget.h"
 #include <QApplication>
-
+#include <QtWidgets>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+
+    QPushButton* pcmdA = new QPushButton("A");
+    QPushButton* pcmdB = new QPushButton("B");
+    QPushButton* pcmdC = new QPushButton("C");
+    QPushButton* pcmdD = new QPushButton("D");
+
+    QVBoxLayout* pvbxLayout = new QVBoxLayout;
+    QHBoxLayout* phbxLayout = new QHBoxLayout;
+ //   phbxLayout->setContentsMargins(5, 5, 5, 5);
+ //   phbxLayout->setSpacing(15);
+    phbxLayout->addWidget(pcmdC);
+    phbxLayout->addWidget(pcmdD);
+
+  //  pvbxLayout->setMargin(5);
+ //   pvbxLayout->setSpacing(15);
+    pvbxLayout->addWidget(pcmdA);
+    pvbxLayout->addWidget(pcmdB);
+    pvbxLayout->addLayout(phbxLayout);
+    w.setLayout(pvbxLayout);
+
+
     w.show();
 
     return a.exec();
